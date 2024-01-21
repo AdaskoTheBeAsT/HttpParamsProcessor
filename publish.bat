e9736454ae430522baf463a10c8970b675859558
Adam Pluciński <adaskothebeast@gmail.com>
2024-01-21 16:14:39 +0100

    upgraded libs

--- publish.bat
+++ publish.bat
@@ -1,4 +1,4 @@
 xcopy .\LICENSE .\dist\libs\http-params-processor\ /Y
 xcopy .\README.md .\dist\libs\http-params-processor\ /Y
 cd dist/libs/http-params-processor
-npm publish --tag=latest --access public
+REM npm publish --tag=latest --access public
