8a2e38bf7f2fd8d6894e3a2568bdae9559502f20
Adam Pluciński <adaskothebeast@gmail.com>
2026-01-11 01:36:50 +0100

    fix in export

--- libs/http-params-processor-angular-resource/src/index.ts
+++ libs/http-params-processor-angular-resource/src/index.ts
@@ -1,6 +1,9 @@
 export {
   httpResourceWithParams,
   reactiveHttpResourceWithParams,
+} from './lib/http-resource-with-params';
+
+export type {
   HttpResourceWithParamsOptions,
   ReactiveHttpResourceWithParamsOptions,
 } from './lib/http-resource-with-params';
